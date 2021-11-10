c078ccfd0dcfde14fb78f340e31c786f43040c61
hye_ppy <1569369@naver.com>
2021-11-10 23:30:48 +0900

    feat: Search Service

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -1,24 +1,35 @@
 //SERVER ROUTES
 export const USER_SERVER = '/api/users';
 
-export const ArrivalsService_URL =
-  'http://apis.data.go.kr/1262000/CountryOverseasArrivalsService/getCountryOverseasArrivalsList'; // 국가별 해외입국자 조치현황
-
 export const API_ENCODED_KEY =
   'r1%2FX7uK7cJ0DtLUvgK%2F9c95mWzoTl6toUTh7YKCij2%2BGRd8OaBD%2FjNqTcjoxP8uJTshSyggHh4ZIBjXCk1qGYA%3D%3D';
 
 export const API_DECODED_KEY =
   'r1/X7uK7cJ0DtLUvgK/9c95mWzoTl6toUTh7YKCij2+GRd8OaBD/jNqTcjoxP8uJTshSyggHh4ZIBjXCk1qGYA==';
 
-// var url =
-//   "http://apis.data.go.kr/1262000/CountryOverseasArrivalsService/getCountryOverseasArrivalsList"; // 국가별 해외입국자 조치현황
 export const SafetyNews_URL =
   'http://apis.data.go.kr/1262000/CountryCovid19SafetyServiceNew/getCountrySafetyNewsListNew'; // 국가 지역별 최신안전소식(코로나관련)
-// var url =
-//   "http://apis.data.go.kr/1262000/CountryKoreaDepartureService/getCountryKoreaDepartureList"; // 국가별 한국발 입국자 조치
-// var url =
-//   "http://apis.data.go.kr/1262000/TravelAlarmService2/getTravelAlarmList2"; // 국가∙지역별 여행경보
-// var url =
-//   "http://apis.data.go.kr/1262000/TravelWarningService/getTravelWarningList"; // 여행경보제도
+
+export const ArrivalsService_URL =
+  'http://apis.data.go.kr/1262000/CountryOverseasArrivalsService/getCountryOverseasArrivalsList'; // 국가별 해외입국자 조치현황
+
+export const EntranceVisaService_URL =
+  'http://apis.data.go.kr/1262000/EntranceVisaService2/getEntranceVisaList2'; // 국가·지역별 입국허가요건
+
+export const KoreaDepartureService_URL =
+  'http://apis.data.go.kr/1262000/CountryKoreaDepartureService/getCountryKoreaDepartureList'; // 국가별 한국발 입국자 조치
+
 export const getCovid19NatInfStateJson_URL =
-  'http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19NatInfStateJson';
+  'http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19NatInfStateJson'; // 보건복지부 코로나19해외발생 현황
+
+export const getTravelAlarm_URL =
+  'http://apis.data.go.kr/1262000/TravelAlarmService2/getTravelAlarmList2'; // 국가∙지역별 여행경보
+
+export const TravelWarning_URL =
+  'http://apis.data.go.kr/1262000/TravelWarningService/getTravelWarningList'; // 여행경보제도 xml
+
+export const TravelWarningInfo_URL =
+  'http://apis.data.go.kr/1262000/TravelWarningService/getTravelWarningInfo'; // 여행경보제도 단일 xml
+
+export const SptravelWarningService_URL =
+  'http://apis.data.go.kr/1262000/SptravelWarningService2/getSptravelWarningList2'; // 국가∙지역별 특별여행주의보
--- client/src/components/views/SearchPage.js
+++ client/src/components/views/SearchPage.js
@@ -1,49 +1,172 @@
 import Page from 'components/Page';
 import React, { useEffect, useState } from 'react';
+import { MdSearch, MdCoronavirus, MdAirplanemodeActive } from 'react-icons/md';
 import SearchInput from 'components/SearchInput';
-import { Row, Col, Button } from 'reactstrap';
+import { NumberWidget, IconWidget } from '../../components/Widget';
 import {
+  Row,
+  Col,
+  Card,
+  CardHeader,
+  CardBody,
+  Badge,
+  Button,
+  Form,
+  Input,
+  Label,
+} from 'reactstrap';
+import {
+  SafetyNews_URL,
   ArrivalsService_URL,
   API_ENCODED_KEY,
   getCovid19NatInfStateJson_URL,
+  EntranceVisaService_URL,
+  KoreaDepartureService_URL,
+  getTravelAlarm_URL,
+  TravelWarning_URL,
+  TravelWarningInfo_URL,
+  SptravelWarningService_URL,
 } from '../Config';
 import Axios from 'axios';
 
 export const SearchPage = props => {
+  const nation = [
+    { iso: 'US', kr: '미국' },
+    { iso: 'NL', kr: '네덜란드' },
+    { iso: 'MK', kr: '북마케도니아' },
+  ];
   const [Info, setInfo] = useState([]);
   const [ReturnType, setReturnType] = useState('JSON');
   const [NumOfRows, setNumOfRows] = useState(10);
   const [PageNo, setPageNo] = useState(1);
-  const [CountryName, setCountryName] = useState('미국');
-  const [CountryIso, setCountryIso] = useState('US');
-  const userId = localStorage.getItem('userId');
-  console.log(userId);
+  const [CountryName, setCountryName] = useState(nation[0].kr);
+  const [CountryIso, setCountryIso] = useState(nation[0].iso);
+  const [InfoVisible, setInfoVisible] = useState(false);
+  const [Title, setTitle] = useState('');
+  const [Content, setContent] = useState('');
+  const [InfoCorona, setInfoCorona] = useState([]);
+  const [Defcnt, setDefcnt] = useState([]);
+
   useEffect(() => {
     window.scrollTo(0, 0);
-    let endpointInfo = `${getCovid19NatInfStateJson_URL}?serviceKey=${API_ENCODED_KEY}&startCreateDt=20211109`;
-    Axios.get(endpointInfo).then(res => {
-      // if (res.data.resultMsg === '정상') {
-      // setInfo(res.data.data);
-      console.log(res.data.response.body.items.item[0]);
-      console.log('gggg');
-      // }
+    let endpointInfo1 = `${getCovid19NatInfStateJson_URL}?serviceKey=${API_ENCODED_KEY}&startCreateDt=20211110`;
+    Axios.get(endpointInfo1).then(res => {
+      setInfoCorona(res.data.response.body.items.item);
     });
   }, []);
+
+  const onCountryNameHandler = event => {
+    setCountryName(event.currentTarget.value);
+  };
+
+  const handleSubmit = async event => {
+    event.preventDefault();
+
+    setInfoVisible(true);
+    console.log(4444444444, CountryName, CountryIso);
+    getArrivalsServiceInfo();
+  };
+  useEffect(() => {
+    const filterNameToIso = name => {
+      nation
+        .filter(item => item.kr.indexOf(name) != -1)
+        .map(data => {
+          setCountryIso(data.iso);
+        });
+    };
+    const filterCnt = name => {
+      InfoCorona.filter(item => item.nationNm.indexOf(name) != -1).map(data => {
+        console.log(data);
+      });
+    };
+    filterNameToIso(CountryName);
+    filterCnt(CountryName);
+  }, [CountryName]);
+
+  const getArrivalsServiceInfo = () => {
+    let endpointInfo =
+      `${ArrivalsService_URL}?serviceKey=${API_ENCODED_KEY}&returnType=${ReturnType}
+    &numOfRows=${NumOfRows}&pageNo=${PageNo}&cond[country_nm::EQ]=` +
+      encodeURI(`${CountryName}`) +
+      `&cond[country_iso_alp2::EQ]=${CountryIso}`;
+    Axios.get(endpointInfo).then((res, body) => {
+      if (res.data.resultMsg === '정상') {
+        console.log(111, res.data.data);
+        res.data.data.map((item, index) => {
+          setTitle(item.title);
+          setContent(item.txt_origin_cn);
+          // console.log(item.title);
+          // console.log(item.txt_origin_cn);
+        });
+      }
+    });
+  };
+
   return (
     <Page
       className="SearchPage"
       title="Search"
       breadcrumbs={[{ name: 'Search', active: true }]}
     >
-      국가명을 입력해주세요.
       <Row>
-        <Col md="6" sm="12" xs="12">
-          <SearchInput />
-        </Col>
-        <Col md="6" sm="12" xs="12">
-          <Button color="secondary">Normal Button</Button>
+        <Col md="6">
+          <Card className="mb-3">
+            <CardHeader>
+              국가명을 입력해주세요.
+              <Form inline className="cr-search-form" onSubmit={handleSubmit}>
+                <Input
+                  type="select"
+                  name="select"
+                  value={CountryName}
+                  onChange={onCountryNameHandler}
+                >
+                  {nation.map((data, index) => {
+                    return <option>{data.kr}</option>;
+                  })}
+                </Input>
+                &nbsp;&nbsp;&nbsp;
+                <Button color="secondary" onClick={handleSubmit}>
+                  검색
+                </Button>
+              </Form>
+            </CardHeader>
+          </Card>
         </Col>
       </Row>
+      {InfoVisible && (
+        <Row>
+          <Col key={1} lg={4} md={6} sm={6} xs={12} className="mb-3">
+            <NumberWidget
+              title="코로나 감염 현황"
+              subtitle="금일"
+              number="사망자수"
+              color="red"
+              progress={{
+                value: 75,
+                label: '사망률',
+              }}
+            />
+          </Col>
+          <Col lg={4} md={6} sm={6} xs={12} className="mb-3">
+            <IconWidget
+              bgColor={'primary'}
+              icon={MdAirplanemodeActive}
+              title="gg"
+              subtitle="ggg"
+            />
+          </Col>
+        </Row>
+      )}
+      {InfoVisible && (
+        <Row>
+          <Col>
+            <Card className="mb-3">
+              <CardHeader>{Title} </CardHeader>
+              <CardBody>{Content}</CardBody>
+            </Card>
+          </Col>
+        </Row>
+      )}
     </Page>
   );
 };
