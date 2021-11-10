5142b81fad4a58c6bdd7247afc07d452b11c2ff3
hye_ppy <1569369@naver.com>
2021-11-11 02:02:04 +0900

    chore: Data processing

--- client/src/components/tasks/LoadCountriesTask.js
+++ client/src/components/tasks/LoadCountriesTask.js
@@ -1,19 +1,41 @@
-import {features} from "assets/geo-data/countries.json"
+import { features } from 'assets/geo-data/countries.json';
 import React, { useEffect, useState } from 'react';
 import { API_ENCODED_KEY, getCovid19NatInfStateJson_URL } from '../Config';
 import Axios from 'axios';
 
 class LoadCountriesTask {
-    setState = null;
-    mapCountries = features;
+  setState = null;
+  mapCountries = features;
+  Defcnt = [];
 
-    load = (setState) =>{
-        Axios.get(`${getCovid19NatInfStateJson_URL}?serviceKey=${API_ENCODED_KEY}&startCreateDt=20211111`)
-        .then(res => {
-            console.log(res);
-        });
-        setState(features);
-    }
-};
+  filtercnt = nation => {
+    this.cnt = 0;
+    this.Defcnt.filter(item => item.nationNmEn.indexOf(nation) != -1).map(
+      item => {
+        console.log(item);
+        this.cnt = item.natDefCnt;
+      },
+    );
+    return this.cnt;
+  };
+
+  load = async setState => {
+    await Axios.get(
+      `${getCovid19NatInfStateJson_URL}?serviceKey=${API_ENCODED_KEY}&startCreateDt=20211110`,
+    ).then(res => {
+      //   console.log(res.data.response.body.items.item);
+      this.Defcnt = res.data.response.body.items.item;
+    });
+    console.log(this.Defcnt);
+    this.mapCountries.map((item, index) => {
+      item.properties = {
+        ...item.properties,
+        natDefCnt: this.filtercnt(item.properties.ADMIN),
+      };
+    });
+    console.log(this.mapCountries);
+    setState(features);
+  };
+}
 
 export default LoadCountriesTask;
