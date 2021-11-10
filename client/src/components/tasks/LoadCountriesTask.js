b79b2c8c87d2755ca4bd5d35bf24f4a6dbb590cc
DESKTOP-1EEHAE2\LGUser <youngine012@naver.com>
2021-11-11 04:30:50 +0900

    feat: extract numbers

--- client/src/components/tasks/LoadCountriesTask.js
+++ client/src/components/tasks/LoadCountriesTask.js
@@ -12,7 +12,7 @@ class LoadCountriesTask {
     this.cnt = 0;
     this.Defcnt.filter(item => item.nationNmEn.indexOf(nation) != -1).map(
       item => {
-        console.log(item);
+        //console.log(item);
         this.cnt = item.natDefCnt;
       },
     );
@@ -20,22 +20,51 @@ class LoadCountriesTask {
   };
 
   load = async setState => {
+    this.setState = setState;
+
     await Axios.get(
       `${getCovid19NatInfStateJson_URL}?serviceKey=${API_ENCODED_KEY}&startCreateDt=20211110`,
     ).then(res => {
       //   console.log(res.data.response.body.items.item);
       this.Defcnt = res.data.response.body.items.item;
     });
-    console.log(this.Defcnt);
+    //console.log(this.Defcnt);
+    
     this.mapCountries.map((item, index) => {
       item.properties = {
         ...item.properties,
         natDefCnt: this.filtercnt(item.properties.ADMIN),
       };
-    });
+    })
+    this.#processCovidData(this.mapCountries);
     console.log(this.mapCountries);
-    setState(features);
   };
+  
+  
+  #processCovidData = (covidCountries) => {
+      for(let i=0; i<this.mapCountries.length;i++){
+          const mapCountry = this.mapCountries[i];
+          const covidCountry = covidCountries.find(
+              (covidCountry) => covidCountry.properties.ISO_A3 === mapCountry.properties.ISO_A3
+        );
+
+        mapCountry.properties.confirmed = 0;
+        mapCountry.properties.confirmedText = "0";
+
+        if(covidCountry != null){
+            const confirmed = Number(covidCountry.properties.natDefCnt);
+            mapCountry.properties.confirmed = confirmed;
+            mapCountry.properties.confirmedText = this.#formatNumberWithCommas(confirmed);
+        }
+    }
+
+    this.setState(this.mapCountries);
+  };
+
+    #formatNumberWithCommas = (number) => {
+        return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    };
+
 }
 
 export default LoadCountriesTask;
