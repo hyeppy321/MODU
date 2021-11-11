b608ebeca6f30479633264744b679d2e320a6ea9
DESKTOP-1EEHAE2\LGUser <youngine012@naver.com>
2021-11-11 15:30:47 +0900

    chore : devide chart

--- client/src/components/chart/CovidChart.js
+++ client/src/components/chart/CovidChart.js
@@ -1,72 +1,16 @@
 import React from 'react';
-import { Card, CardBody, CardHeader, Col, Row} from 'reactstrap';
-import Page from 'components/Page';
-import { getColor } from 'utils/colors';
-import { randomNum } from 'utils/demos';
-import { Line, Pie, Doughnut, Bar, Radar, Polar } from 'react-chartjs-2';
-
-
-const MONTHS = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-
-const genLineData = (moreData = {}, moreData2 = {}) => {
-  return {
-    labels: MONTHS,
-    datasets: [
-      {
-        label: 'Dataset 1',
-        backgroundColor: getColor('primary'),
-        borderColor: getColor('primary'),
-        borderWidth: 1,
-        data: [
-          randomNum(),
-          randomNum(),
-          randomNum(),
-          randomNum(),
-          randomNum(),
-          randomNum(),
-          randomNum(),
-        ],
-        ...moreData,
-      },
-      {
-        label: 'Dataset 2',
-        backgroundColor: getColor('secondary'),
-        borderColor: getColor('secondary'),
-        borderWidth: 1,
-        data: [
-          randomNum(),
-          randomNum(),
-          randomNum(),
-          randomNum(),
-          randomNum(),
-          randomNum(),
-          randomNum(),
-        ],
-        ...moreData2,
-      },
-    ],
-  };
-};
+import {Col, Row} from 'reactstrap';
+import Chart1 from './Chart1';
+import Chart2 from './Chart2';
 
 const CovidChart = () => {
     return (
         <Row>
         <Col xl={6} lg={12} md={12}>
-          <Card>
-            <CardHeader>Chart 1</CardHeader>
-            <CardBody>
-              <Bar data={genLineData()} />
-            </CardBody>
-          </Card>
+          <Chart1/>
         </Col>
-
         <Col xl={6} lg={12} md={12}>
-          <Card>
-            <CardHeader>Chart 2</CardHeader>
-            <CardBody>
-              <Line data={genLineData({ fill: false }, { fill: false })} />
-            </CardBody>
-          </Card>
+          <Chart2/>
         </Col>
         </Row>
      );
