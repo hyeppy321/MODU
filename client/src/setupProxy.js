292386e0e52aa2fa349138fe70e6fa203719eaa1
DESKTOP-1EEHAE2\LGUser <youngine012@naver.com>
2021-11-26 13:05:32 +0900

    fix: change target

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://3.13.120.46:5000',
+      target: 'http://allcoronainfo.co.kr:5000',
       changeOrigin: true,
     }),
   );
